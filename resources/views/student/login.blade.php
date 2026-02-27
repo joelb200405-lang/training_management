2ffeca056dd40d215e13d37709fac9bae4c960ff
BargainInTheParkingLot <148935919+BargainInTheParkingLot@users.noreply.github.com>
2026-02-27 19:46:23 +0800

    commit

---
+++ resources/views/student/login.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('stylesheet/login.css') }}">
+
+    <!-- bootstrap link -->
+    <link rel="stylesheet" href="../bootstrap_folder/css/bootstrap.min.css">
+    
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="../font-awesome-icon/css/all.min.css">
+</head>
+
+<body>
+
+    <div class="container-fluid p-0">
+
+        <form action="?url=login" method="post">
+
+            <div class="login-parent">
+                <div class="form-extension image-container">
+                    <img src="../../images/bead.jpg" alt="bread">
+                </div>
+                <div class="form-extension form">
+
+                    <div class="form-logo">
+                        <div class="parent-logo">
+                            <div class="logo"></div>
+                            <h2>Livelihood Program</h2>
+                        </div>
+                        <h2 class="fw-semibold">Let's the learning journey begin.</h2>
+                        <p>Unlock a world of learning with just one click. Log in to get started.</p>
+                    </div>
+
+                    <div class="text-box">
+                        <label>Username or Email</label>
+                        <input type="text" class="form-control" placeholder="Enter username or email" name="email" id=""
+                            required>
+                    </div>
+
+                    <div class="text-box">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Enter password" name="password" id=""
+                            required>
+                    </div>
+
+                    <div class="forgot text-end">
+                        <a href="#">Forgot Password</a>
+                    </div>
+
+                    <div class="button">
+                        <button class="submit">Login</button>
+                    </div>
+
+                    <h4>Or Login with</h4>
+
+                    <div class="accounts">
+                        <a href="#">
+                            <i class="fa-brands fa-google"></i>
+                            <p class="m-0">Google</p>
+                        </a>
+
+                        <a href="#">
+                            <i class="fa-brands fa-facebook"></i>
+                            <p class="m-0">Facebook</p>
+                        </a>
+                    </div>
+
+                    <div class="change">
+                        <label>Don't have an account? <a href="/sign_up">Signup</a>
+                        </label>
+                    </div>
+
+                </div>
+
+            </div>
+        </form>
+    </div>
+
+    <!-- bootstrap link javascript -->
+    <script defer src="../bootstrap_folder/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,18 @@ Route::get('/', function () {
     return view('student.landingpage');
 });
 
-Route::get("/about-page", [product_controller::class, "about_page"])->name("about_page");
+Route::get('/login', function (){
+    return view('student.login');
+});
+Route::get('/sign_up', function (){
+    return view('student.sign_up');
+});
 
-Route::post("/insert-data", [product_controller::class, "InsertData"])->name("InsertData");
+// Route::get("/about-page", [product_controller::class, "about_page"])->name("about_page");
+
+// Route::post("/insert-data", [product_controller::class, "InsertData"])->name("InsertData");
 
 // Route::get("/about-page", function () {
 //     return view("about");
 // })->name("about");
+?>
